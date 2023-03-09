1b6d6ddbddd112c8b77589fbd9a8ddba2bd5512a
Brettell <thomas.a.brettell@gmail.com>
2023-03-09 11:36:50 +1100

    add smoothstep to lerp, states wip

--- src/components/Scene/index.js
+++ src/components/Scene/index.js
@@ -10,45 +10,43 @@ import gsap, { TweenLite } from 'gsap';
 import Color from 'color';
 import { useFrame } from '@react-three/fiber';
 import * as THREE from 'three';
-import { clamp, lerp } from 'three/src/math/MathUtils';
+import { clamp, lerp, smootherstep, smoothstep } from 'three/src/math/MathUtils';
 
 const radius = 1;
 const padding = radius * 0.25;
 
 const COUNTRIES = ['CHINA', 'AUSTRALIA', 'INDIA'];
 
-const COUNTRY_COLORS = {
-  CHINA: 'red',
-  AUSTRALIA: 'green',
-  INDIA: 'blue'
+const COUNTRY_DETAILS = {
+  CHINA: {
+    color: 'red'
+  },
+  AUSTRALIA: {
+    color: 'green'
+  },
+  INDIA: {
+    color: 'blue'
+  }
 };
 
-const pointsAmount = 100000;
+const pointsAmount = 80000;
 
-const circles = Array.from({ length: pointsAmount }).map(() => ({
-  r: radius + padding,
-  country: COUNTRIES[Math.floor(Math.random() * COUNTRIES.length)]
-}));
+const points = Array.from({ length: pointsAmount }).map(() => {
+  return {
+    r: radius + padding,
+    country: Object.keys(COUNTRY_DETAILS)[Math.floor(Math.random() * Object.keys(COUNTRY_DETAILS).length)]
+  };
+});
 
-const pack = d3.packSiblings(circles);
+const STATES = ['Circle 1', 'Circle 2', 'Rectangles'];
+
+const pack = d3.packSiblings(points);
 const pack2 = shuffle(pack);
 
 const center = d3.packEnclose(pack);
 
-// const makeIntoArray = (a) => {
-//   const array = []
-//   for(let i = 0,)
-// }
-
-const initialPositions = new Float32Array(pack.length * 3);
-for (let i = 0; i < pack.length; i++) {
-  initialPositions[i * 3] = pack[i].x + center.r;
-  initialPositions[i * 3 + 1] = pack[i].y + center.r;
-  initialPositions[i * 3 + 2] = 0;
-}
-
 const colours = Float32Array.from(
-  new Array(pack.length).fill().flatMap((_, i) => Color(COUNTRY_COLORS[pack[i].country]).array())
+  new Array(pack.length).fill().flatMap((_, i) => Color(COUNTRY_DETAILS[pack[i].country].color).array())
 );
 
 let isPack1 = true;
@@ -64,17 +62,15 @@ const Scene = () => {
   const { width: windowWidth, height: windowHeight } = useWindowSize();
 
   useFrame((state, deltaTime) => {
-    interpolateDuration.current = clamp((interpolateDuration.current += deltaTime), 0, 1);
+    interpolateDuration.current = clamp((interpolateDuration.current += deltaTime * 0.99), 0, 1);
 
     for (let i = 0; i < currentPositions.current.length; i++) {
       const pos = currentPositions.current[i];
       const toPos = newPositions.current[i];
 
-      o.position.set(
-        lerp(pos.x, toPos.x, interpolateDuration.current),
-        lerp(pos.y, toPos.y, interpolateDuration.current),
-        0
-      );
+      const st = smoothstep(interpolateDuration.current, 0, 1);
+
+      o.position.set(lerp(pos.x, toPos.x, st), lerp(pos.y, toPos.y, st), 0);
       o.updateMatrix();
       instanceRef.current.setMatrixAt(i, o.matrix);
     }
@@ -82,41 +78,22 @@ const Scene = () => {
   });
 
   const tweenHandler = () => {
+    interpolateDuration.current = Math.abs(interpolateDuration.current - 1);
     if (isPack1) {
       isPack1 = false;
       newPositions.current = pack2;
       currentPositions.current = pack;
-      interpolateDuration.current = 0;
     } else {
       isPack1 = true;
       newPositions.current = pack;
       currentPositions.current = pack2;
-      interpolateDuration.current = 0;
     }
-    // const newPositions = new Float32Array(positions.length);
-    // const shuffledPack = shuffle(pack);
-
-    // for (let i = 0; i < shuffledPack.length; i++) {
-    //   newPositions[i * 3] = shuffledPack[i].x + center.r;
-    //   newPositions[i * 3 + 1] = shuffledPack[i].y + center.r;
-    //   newPositions[i * 3 + 2] = 0;
-    // }
-
-    // const animationObject = positions;
-    // newPositions.onUpdate = function () {
-    //   const newArray = new Float32Array(animationObject.length);
-    //   newArray.set(animationObject);
-    //   setPositions(newArray);
-    // };
-
-    // TweenLite.to(animationObject, 1, newPositions);
   };
 
   return (
     <>
       <group position={[-windowWidth / 2, windowHeight / 2, 0]}>
         <group position={[center.r, -center.r, 0]}>
-          {/* <Circle x={center.r} y={center.r} r={center.r} /> */}
           <InstancedCicles
             ref={instanceRef}
             length={currentPositions.current.length}
@@ -124,10 +101,7 @@ const Scene = () => {
             radius={radius}
           />
         </group>
-
-        {/* {pack.map((d, i) => (
-          <Circle key={i} x={d.x + center.r} y={d.y + center.r} r={radius} color={'red'} />
-        ))} */}
+        <Square x={windowWidth - 75} y={175} width="100" height={100} />
       </group>
 
       <Stats />
@@ -145,6 +119,12 @@ const Scene = () => {
           <div>Points: {pointsAmount}</div>
           <div>Radius: {radius}</div>
           <div>Padding: {padding}</div>
+
+          <div style={{ display: 'flex', flexDirection: 'column', marginTop: '10px' }}>
+            {STATES.map(state => (
+              <button key={state}>{state}</button>
+            ))}
+          </div>
         </div>
       </Html>
     </>
